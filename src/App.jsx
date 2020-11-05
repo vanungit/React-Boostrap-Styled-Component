ddb491717ba24c56550efa7113329c26826fa57f
Vanuni Sakanyan <vanunisakanyan@mail.ru>
2020-11-05 23:27:09 +0400

    create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+    return (
+        <div>
+          <Button>asdfds</Button>
+        </div>
+    );
+}
+
+export default App;
